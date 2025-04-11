6fe47eab732d18ecdb81264f5b7245d90be3bded
OwenC14220326 <c14220326@john.petra.ac.id>
2025-04-11 18:43:55 +0700

    balik vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/c14220326_owen_uts_csp/',
+  plugins: [react()],
 })
